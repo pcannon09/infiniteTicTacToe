222183dbcc73912b464562e316623fe1a0ceb84c
pcannon09 <pcannon775@gmail.com>
2025-01-10 14:41:00 +0100

    Created type files

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip3 install pygame --break-system-packages
+pip3 install colorama --break-system-packages
+
---
+++ src/main.py
@@ -0,0 +1,12 @@
+from types.Cross import Cross
+from types.Circle import Circle
+from types.SquarePos import SquarePos
+
+import utils
+
+def main():
+    pass
+
+if (__name__ == "__main__"):
+    main()
+
