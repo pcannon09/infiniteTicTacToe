bb73212972a0a28e3a2255d2d29d40738f2ce941
pcannon09 <pcannon775@gmail.com>
2025-01-14 15:57:36 +0100

    Added commands to install python package(s)

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-pip3 install pygame --break-system-packages
-pip3 install colorama --break-system-packages
+if [[ ! -d "pip_modules" ]]; then
+    echo "[ * ] Creating \`pip_modules/\`"
+    mkdir pip_modules/
+    echo "[ OK ]"
+fi
+
+echo "[ * ] Installing \`pygame\`" ; pip3 install --target=./pip_modules pygame ; echo "[ OK ]"
+echo "[ * ] Installing \`colorama\`" ; pip3 install --target=./pip_modules colorama ; echo "[ OK ]"
+
+echo "[ DONE ]"
 
