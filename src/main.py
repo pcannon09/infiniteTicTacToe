1ea51321069029d6568b6191719aee38dcb9ad8c
pcannon09 <pcannon775@gmail.com>
2025-01-26 19:26:56 +0100

    Testing with Debug and other stuff

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from config import *
 
-from types.Cross import Cross
-from types.Circle import Circle
-from types.SquarePos import SquarePos
+# from types.Cross import Cross
+# from types.Circle import Circle
+# from types.SquarePos import SquarePos
 
-import utils
+from utils import Debug
+
+from colorama import Fore
+
+import pygame
+
+import var
+
+if (var.DEV):
+    print(f"{Fore.LIGHTCYAN_EX}[ * ] Initalized as DEV mode")
+
+pygame.init()
+
+debug = Debug("./.private/logs/main-logs.txt")
 
 def main():
     pass
