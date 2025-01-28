e431e1e12d9bdb195125cc1594106b1a1f64fdb6
pcannon09 <pcannon775@gmail.com>
2025-01-28 14:30:25 +0000

    Added some variables

--- src/var.py
+++ src/var.py
@@ -9,3 +9,12 @@ Y = 500
 
 debug: Const = Debug("./.private/logs/main-logs.txt")
 
+class Colors:
+    WHITE: Const = (255, 255, 255)
+    WHITE2: Const = (245, 245, 245)
+
+    BLACK: Const = (0, 0, 0)
+    JETBLACK: Const = (30, 30, 30)
+
+    GREY: Const = (155, 155, 155)
+
