68f201871407564604ae12be5e32cdc91137d44c
pcannon09 <pcannon775@gmail.com>
2025-01-28 14:35:06 +0000

    Created 'SquarePos' class variables and rendered them on the window

--- src/main.py
+++ src/main.py
@@ -1,27 +1,47 @@
 from config import *
+from var import *
 
-# from types.Cross import Cross
-# from types.Circle import Circle
-# from types.SquarePos import SquarePos
+# from Cross import Cross
+# from Circle import Circle
+from SquarePos import SquarePos
 
 from colorama import Fore
 
 import pygame
-
-from var import *
 
 if (DEV):
     print(f"{Fore.LIGHTCYAN_EX}[ * ] Initalized as DEV mode{Fore.RESET}")
 
 pygame.init()
 
-window = pygame.display.set_mode((X, Y))
+window = pygame.display.set_mode((X, Y), flags=pygame.RESIZABLE)
+
+sqpos1: SquarePos = SquarePos(window, "sq1", 1,
+                            20, True, 5, Colors.JETBLACK)
+
+sqpos2: SquarePos = SquarePos(window, "sq2", 2,
+                            20, True, 5, Colors.JETBLACK)
+
+def mainUpdate():
+    global X, Y
+
+    X, Y = window.get_size()
+
+    window.fill(Colors.GREY)
 
 def main():
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             pygame.quit()
-            sys.exit(0);
+            sys.exit(0)
+
+    mainUpdate()
+
+    # MAIN GAME
+    sqpos1.render(10, 10, 100, 100)
+    sqpos2.render(10 + 150, 10, 100, 100)
+
+    pygame.display.update()
 
 if (__name__ == "__main__"):
     while 1:
