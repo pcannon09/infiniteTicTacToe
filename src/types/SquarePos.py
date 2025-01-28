8dc0a5854f06fd29bde1286c5e2d9422bf708009
pcannon09 <pcannon775@gmail.com>
2025-01-28 14:35:52 +0000

    Added functions such as: setColor(), setFill(), render() and more

--- src/types/SquarePos.py
+++ src/types/SquarePos.py
@@ -1,2 +1,35 @@
+from typing import Final as Const
+
+import pygame
+
 class SquarePos:
-    pass
+    def __init__(self, window: pygame.Surface, id: str, pos: int, br: int = 10, fill: bool = False, borderThickness: int = 5,
+                 color: tuple = (0, 0, 0)):
+        self.window = window
+
+        self.id: Const = id
+        self.pos: Const = pos
+
+        self.borderRad = br
+        self.fill = fill
+        self.color = color
+        self.borderThickness =  borderThickness
+
+        self.x = 0
+        self.y = 0
+        self.w = 0
+        self.h = 0
+
+    def setColor(self, color: tuple): self.color = color
+    def setThickness(self, thickness: int): self.borderThickness = thickness
+    def setFill(self, fill: bool): self.fill = fill
+
+    def render(self, x: int, y: int, w: int, h: int):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+        pygame.draw.rect(self.window, self.color, (self.x, self.y,
+                                                   self.w, self.h), self.borderThickness, border_radius=self.borderRad)
+
