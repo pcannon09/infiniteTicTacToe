85468ead77a74ba7dd5f0744151e9257d5b4dfc3
pcannon09 <pcannon775@gmail.com>
2025-01-30 15:53:08 +0000

    Added variables to do their stuff

--- src/main.py
+++ src/main.py
@@ -1,33 +1,41 @@
 from config import *
-from var import *
 
-# from Cross import Cross
-# from Circle import Circle
+from Players import Players
 from SquarePos import SquarePos
+from Shapes import Shapes
+
+import var
 
 from colorama import Fore
 
 import pygame
 
-if (DEV):
-    print(f"{Fore.LIGHTCYAN_EX}[ * ] Initalized as DEV mode{Fore.RESET}")
+if (var.DEV):
+    var.sysDebug.debug(f"{Fore.LIGHTCYAN_EX}[ * ] Initalized as DEV mode{Fore.RESET}", "info")
 
 pygame.init()
 
-window = pygame.display.set_mode((X, Y), flags=pygame.RESIZABLE)
+window = pygame.display.set_mode((var.X, var.Y), flags=pygame.RESIZABLE)
+
+players: Players = Players("main-players", 1, True)
 
 sqpos1: SquarePos = SquarePos(window, "sq1", 1,
-                            20, True, 5, Colors.JETBLACK)
+                            20, True, 5, var.Colors.JETBLACK)
 
 sqpos2: SquarePos = SquarePos(window, "sq2", 2,
-                            20, True, 5, Colors.JETBLACK)
+                            20, True, 5, var.Colors.JETBLACK)
+
+sqpos3: SquarePos = SquarePos(window, "sq3", 3,
+                            20, True, 5, var.Colors.JETBLACK)
+
+cross: Shapes = Shapes(window, "main-shapes", "cross")
 
 def mainUpdate():
     global X, Y
 
     X, Y = window.get_size()
 
-    window.fill(Colors.GREY)
+    window.fill(var.Colors.GREY)
 
 def main():
     for event in pygame.event.get():
@@ -38,12 +46,17 @@ def main():
     mainUpdate()
 
     # MAIN GAME
-    sqpos1.render(10, 10, 100, 100)
-    sqpos2.render(10 + 150, 10, 100, 100)
+    sqpos1.render(10, 10,                         var.squareSize, var.squareSize)
+    sqpos2.render(10 + var.squareSpacing, 10,     var.squareSize, var.squareSize)
+    sqpos3.render(10 + var.squareSpacing * 2, 10, var.squareSize, var.squareSize)
+
+    cross.render(sqpos1)
 
     pygame.display.update()
 
 if (__name__ == "__main__"):
+    var.sysDebug.debug("", "info")
+
     while 1:
         main()
 
