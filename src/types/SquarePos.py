b077c21d86399a6f01f6bc7a0f28d6ac02ebc10d
pcannon09 <pcannon775@gmail.com>
2025-01-30 15:54:40 +0000

    Minor changes

--- src/types/SquarePos.py
+++ src/types/SquarePos.py
@@ -1,10 +1,12 @@
 from typing import Final as Const
 
+import var
+
 import pygame
 
 class SquarePos:
     def __init__(self, window: pygame.Surface, id: str, pos: int, br: int = 10, fill: bool = False, borderThickness: int = 5,
-                 color: tuple = (0, 0, 0)):
+                 color: tuple = (0, 0, 0)) -> None:
         self.window = window
 
         self.id: Const = id
@@ -20,11 +22,13 @@ class SquarePos:
         self.w = 0
         self.h = 0
 
-    def setColor(self, color: tuple): self.color = color
-    def setThickness(self, thickness: int): self.borderThickness = thickness
-    def setFill(self, fill: bool): self.fill = fill
+        var.sysDebug.debug(f"Init `SquarePos` with ID: {id}", "info")
 
-    def render(self, x: int, y: int, w: int, h: int):
+    def setColor(self, color: tuple) -> None: self.color = color
+    def setThickness(self, thickness: int) -> None: self.borderThickness = thickness
+    def setFill(self, fill: bool) -> None: self.fill = fill
+
+    def render(self, x: int, y: int, w: int, h: int) -> None:
         self.x = x
         self.y = y
         self.w = w
@@ -32,4 +36,20 @@ class SquarePos:
 
         pygame.draw.rect(self.window, self.color, (self.x, self.y,
                                                    self.w, self.h), self.borderThickness, border_radius=self.borderRad)
+
+    def getClick(self) -> bool:
+        global mouse
+
+        mouse = pygame.mouse.get_pressed()
+
+        if (mouse[0]): # Left click (0), Right click (1)
+            mx: int = pygame.mouse.get_pos()[0]
+            my: int = pygame.mouse.get_pos()[1]
+            
+            if (mx >= self.x and mx <= self.x + self.w and
+                my >= self.y and my <= self.y + self.h):
+                
+                return True
+
+        return False
 
