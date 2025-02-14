45524fe016e6ff98dc8fe7212a09048f261861d9
pcannon09 <pcannon775@gmail.com>
2025-02-14 12:15:26 +0000

    Very minor changes

--- src/types/Players.py
+++ src/types/Players.py
@@ -3,16 +3,17 @@ from typing import Final as Const
 from var import sysDebug
 
 class Players:
-    def __init__(self, id: str, startPlayer: int = 1, playerIsCircle: bool = False):
+    def __init__(self, id: str, playerShape: str, startPlayer: int = 1):
         self.id: Const = id
 
-        self.playerIsCircle = playerIsCircle
-        self.player = startPlayer
+        self.playerShape: str = playerShape
+
+        self.currentPlayer: int = startPlayer
         
         sysDebug.debug(f"Init `Players` with ID: {id}")
 
     def switch(self):
-        self.player = 2 if self.player == 1 else 1
+        self.currentPlayer = 2 if self.currentPlayer == 1 else 1
         
-        sysDebug.debug(f"Player switched to `{self.player}` - ID: {self.id}")
+        sysDebug.debug(f"Player switched to `{self.currentPlayer}` - ID: {self.id}")
 
