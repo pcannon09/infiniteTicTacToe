2316032c5c2f4de49964d1ff076fb1b97b55b995
pcannon09 <pcannon775@gmail.com>
2025-03-25 14:56:45 +0000

    Fixed some bugs

--- src/types/Shapes.py
+++ src/types/Shapes.py
@@ -8,13 +8,14 @@ class ErrorUnknownShape(Exception):
     pass
 
 class Shapes:
-    def __init__(self, window: pygame.Surface, id: str, shape: str | None = None):
+    def __init__(self, window: pygame.Surface, id: str, shapeID: int | None = None, shape: str | None = None):
         self.id: Const = id
         
         self.validShapes: list = ["circle", "cross", "square", "triangle", None]
 
         self.window: pygame.Surface = window
         self.shape: str | None = shape
+        self.shapeID: int | None = shapeID
 
         self.shapePositionSpacing: int = 10
 
@@ -23,29 +24,35 @@ class Shapes:
         if (self.shape not in self.validShapes):
             raise ErrorUnknownShape(f"{self.shape} is not a valid shape, make sure you have a valid one")
 
-        if (self.shape is not None):
-            self._image: pygame.Surface = pygame.image.load(f"rsrc/imgs/players/{self.shape}.png")
+        if (self.shapeID is not None):
+            self.shapeID = shapeID
 
-            self._image = pygame.transform.smoothscale(self._image,
-                                                       ( var.squareSize - ( self.shapePositionSpacing * 2 ), var.squareSize - ( self.shapePositionSpacing * 2 ) ))
+        self.image: pygame.Surface | None = self.set(self.shape)
 
         var.sysDebug.debug(f"LOADED image for `Shapes` with ID: {id}", "info")
         var.sysDebug.debug(f"Init `Shapes` with ID: {id}", "info")
 
-    def set(self, shape: str) -> None:
+    def set(self, shape: str | None) -> pygame.Surface | None:
         self.shape = shape
 
+        image: pygame.Surface | None = None
+
         if (self.shape is not None):
-            self._image: pygame.Surface = pygame.image.load(f"rsrc/imgs/players/{self.shape}.png")
+            image = pygame.image.load(f"rsrc/imgs/players/{self.shape}.png")
+            image = pygame.transform.smoothscale(image,
+                                                 ( var.squareSize - ( self.shapePositionSpacing * 2 ), var.squareSize - ( self.shapePositionSpacing * 2 ) ))
 
-            self._image = pygame.transform.smoothscale(self._image,
-                                                       ( var.squareSize - ( self.shapePositionSpacing * 2 ), var.squareSize - ( self.shapePositionSpacing * 2 ) ))
+            return image
 
-    def render(self, bind):
+        return None
+
+    def render(self, bind: pygame.Surface | None):
         """
         Render
         * Render the enemy to the desired ID binded with `SquarePos`
         """
         
-        self.window.blit(self._image, (bind.x + self.shapePositionSpacing, bind.y + self.shapePositionSpacing))
+        if (self.image is not None):
+            if (bind is not None):
+                self.window.blit(self.image, (bind.x + self.shapePositionSpacing, bind.y + self.shapePositionSpacing))
 
