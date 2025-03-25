315b2675f244cc17af4aaed236c9eeb61271b4cc
pcannon09 <pcannon775@gmail.com>
2025-03-25 14:56:21 +0000

    Testing on rendering the images and other stuff

--- src/types/Players.py
+++ src/types/Players.py
@@ -1,19 +1,33 @@
 from typing import Final as Const
 
 from var import sysDebug
+from Shapes import Shapes
 
 class Players:
-    def __init__(self, id: str, playerShape: str, startPlayer: int = 1):
-        self.id: Const = id
+    def __init__(self, id: str, playerShapes: list, startPlayer: int = 1):
+        self.id: str = id
 
-        self.playerShape: str = playerShape
+        if startPlayer > len(playerShapes):
+            startPlayer = len(playerShapes)
 
+        self.playerShapes: list[Shapes] = playerShapes
         self.currentPlayer: int = startPlayer
-        
+        self.playerShape: Shapes | None = self.playerShapes[self.currentPlayer - 1]
+
         sysDebug.debug(f"Init `Players` with ID: {id}")
 
-    def switch(self):
+    def switch(self) -> int:
         self.currentPlayer = 2 if self.currentPlayer == 1 else 1
         
+        self.playerShape = self.playerShapes[self.currentPlayer - 1]
+
         sysDebug.debug(f"Player switched to `{self.currentPlayer}` - ID: {self.id}")
+
+        return self.currentPlayer
+
+    def render(self):
+        sysDebug.debug(self.playerShape.image, "debug")
+
+        self.playerShape.render(self.playerShape.image)
+
 
